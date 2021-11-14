0b17a48cf9e86a97a803b49978bfdd2fe4f9c6ee
Damian Jarominek <damian.jarominek@gmail.com>
2021-11-14 14:03:56 +0100

    Add TM1637 display

--- RP2040/main.py
+++ RP2040/main.py
@@ -3,13 +3,21 @@ import sys
 import utime
 import machine
 import bme280
+import tm1637
 
 i2c = machine.I2C(0, freq=399361, scl=machine.Pin(1), sda=machine.Pin(0))
 
 
 bme = bme280.BME280(i2c=i2c, address=0x76)
 
+mydisplay = tm1637.TM1637(clk=machine.Pin(7), dio=machine.Pin(6))
+
+mydisplay.scroll("Hello World 123", delay=200)
+utime.sleep(1)
+
 while True:
+    temp = int(float((bme.raw_values[0])))
+    mydisplay.temperature(temp)
     if select.select([sys.stdin], [], [], 0)[0]:
         ch = sys.stdin.readline()
         if ch[0] == "0":
@@ -18,5 +26,5 @@ while True:
             print(bme.raw_values[1] + " hPa")
         if ch[0] == "2":
             print(bme.raw_values[2] + " %")
-
-    utime.sleep(1)
+    
+    utime.sleep(2)
